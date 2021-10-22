2edecc1ccc38dbb13d21afd193f56040c0a997ce
EL LAHIBI ILIASS p1908771 <iliass.el-lahibi@etu.univ-lyon1.fr>
2021-10-22 15:00:26 +0200

    ajout fichier base de donnees 1

---
+++ BD/BD.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS `operation`;
+CREATE TABLE IF NOT EXISTS `operation` (
+  `ID` int(11) NOT NULL ,
+  `formule` varchar(255) DEFAULT NULL,
+  `calcul` varchar(255) DEFAULT NULL,
+  `resultat` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL DEFAULT '0',
+  `password` varchar(50) NOT NULL DEFAULT '0',
+  `quote` varchar(50) NOT NULL DEFAULT '0',
+  `idUser` int(11),
+  PRIMARY KEY (`ID`)
+  CONSTRAINT fk_image_user          -- On donne un nom à notre clé
+        FOREIGN KEY (idUser)             -- Colonne sur laquelle on crée la clé
+        REFERENCES User(ID)
+);
