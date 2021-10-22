f83597de3ab1ffaf96108180302881fef4a38fd2
COLLET Quentin <quentin.collet@soprasteria.com>
2021-10-22 13:48:40 +0200

    route index et image

--- calculator.py
+++ calculator.py
@@ -4,3 +4,9 @@ from markupsafe import escape
 import sqlite3
 from flask import current_app, g
 from flask.cli import with_appcontext
+
+
+@app.route("/")
+# temporaire, à changer une fois qu'on a la bdd
+def index():
+    return render_template("index.html", name="Test name", idimage=1)
